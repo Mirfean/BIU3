0ed5f78570a7b5754c08d27b88fa98af9f4f67d8
Mirfean <mail.random@wp.pl>
2016-11-21 21:52:28 +0100

    Add files via upload

---
+++ Zad2 JS/scripts/scripts.js
@@ -0,0 +1,11 @@
+var viewModel = new PeopleTableViewModel({
+    pageSize: 25,
+    count: data.length,
+    context: document.getElementById('table')
+});
+
+var comparator = new Comparators();
+
+function init() {
+    viewModel.next();
+}
